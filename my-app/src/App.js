b00ece0a96655e84821d52c494df9160eaa53a29
Ivan Morel <ivan.morel@1st.com>
2024-06-16 11:13:38 -0400

    changing text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Klk mi gente
+          klk manito
         </p>
         <a
           className="App-link"
